cd8eb18dffb11f5fabdd84be1730f086ae2977de
Jordan Robinson <j.robinson@building-blocks.com>
2014-08-19 18:43:45 +0100

    First code kata

---
+++ CodeKata/Checkout.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    using System.Diagnostics;
+
+    internal class Checkout
+    {
+
+        public int total;
+
+        private string items = "";
+
+        public void Scan(char code)
+        {
+            items += code;
+            var itemsArray = items.ToCharArray();
+            Array.Sort(itemsArray);
+            items = new string(itemsArray);
+
+
+            if (items.Contains("AAA"))
+            {
+                items = items.Replace("AAA", "Z");
+                total -= 100;
+                code = 'Z';
+            }
+            else if (items.Contains("BB"))
+            {
+                items = items.Replace("BB", "Y");
+                total -= 30;
+                code = 'Y';
+            }
+
+            switch (code)
+            {
+                case 'A':
+                    total += 50;
+                    break;
+                case 'B':
+                    total += 30;
+                    break;
+                case 'C':
+                    total += 20;
+                    break;
+                case 'D':
+                    total += 15;
+                    break;
+                case 'Y':
+                    total += 45;
+                    break;
+                case 'Z':
+                    total += 130;
+                    break;
+                default:
+                    Debug.WriteLine("It done broke");
+                    break;
+            }
+        }
+    }
+}
---
+++ CodeKata/CheckoutTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using NUnit;
+
+namespace CodeKata
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CheckoutTests
+    {
+        private int Price(string goods)
+        {
+            var checkout = new Checkout();
+            foreach (var product in goods.ToCharArray())
+            {
+                checkout.Scan(product);
+            }
+
+            return checkout.total;
+        }
+
+
+        [Test]
+        public void TestOne()
+        {
+            const string ProductCode = "A";
+            
+
+            Assert.AreEqual(50, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            const string ProductCode = "";
+            
+            Assert.AreEqual(0, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestThree()
+        {
+            var ProductCode = "AB";
+            
+            Assert.AreEqual(80, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestFour()
+        {
+            var ProductCode = "CDBA";
+            
+            Assert.AreEqual(115, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestFive()
+        {
+            var ProductCode = "AA";
+            
+            Assert.AreEqual(100, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestSix()
+        {
+            var ProductCode = "AAA";
+            
+            Assert.AreEqual(130, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestSeven()
+        {
+            var ProductCode = "AAAA";
+            
+            Assert.AreEqual(180, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestEight()
+        {
+            var ProductCode = "AAAAA";
+            
+            Assert.AreEqual(230, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestNine()
+        {
+            var ProductCode = "AAAAAA";
+            
+            Assert.AreEqual(260, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestTen()
+        {
+            var ProductCode = "AAAB";
+            
+            Assert.AreEqual(160, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestEleven()
+        {
+            var ProductCode = "AAABB";
+            
+            Assert.AreEqual(175, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestTwelve()
+        {
+            var ProductCode = "AAABBD";
+
+            Assert.AreEqual(190, Price(ProductCode));
+        }
+
+        [Test]
+        public void TestThirteen()
+        {
+            var ProductCode = "DABABA";
+
+            Assert.AreEqual(190, Price(ProductCode));
+        }
+    }
+}
